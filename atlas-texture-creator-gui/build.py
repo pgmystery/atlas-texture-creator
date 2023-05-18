fb1fc39df64f8de020471cec348f7f9d2cbcae10
pglaw <philipp.glaw@gmx.de>
2023-05-18 12:06:59 +0200

    Add GitHub action for building and releasing the application

--- atlas-texture-creator-gui/build.py
+++ atlas-texture-creator-gui/build.py
@@ -1,12 +1,22 @@
+import os
 import PyInstaller.__main__
 
 
 def build():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    dist_path = os.path.join(dir_path, "dist", "atlas-texture-creator")
+
+    if not os.path.exists(dist_path):
+        os.makedirs(dist_path)
+
+
     PyInstaller.__main__.run([
         "--name",
         "Atlas Texture Creator",
         "--onefile",
         "--noconsole",
+        "--distpath",
+        dist_path,
         "run.py",
         "--clean",
     ])
