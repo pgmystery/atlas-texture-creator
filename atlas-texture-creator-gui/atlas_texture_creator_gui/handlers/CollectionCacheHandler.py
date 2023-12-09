db99c5428fb48f2bbf69ffc1d1e0bd753f93c62d
pgmystery <philipp.glaw@gmx.de>
2023-12-09 12:13:54 +0100

    WIP: Fix bug.

--- atlas-texture-creator-gui/atlas_texture_creator_gui/handlers/CollectionCacheHandler.py
+++ atlas-texture-creator-gui/atlas_texture_creator_gui/handlers/CollectionCacheHandler.py
@@ -15,18 +15,43 @@ class CollectionCache(Path):
     def add_texture(self, texture_file_path: str) -> Path:
         file_name = Path(texture_file_path).name
         file_path = Path(self) / file_name
-        shutil.copy(texture_file_path, file_path)
+        try:
+            shutil.copy(texture_file_path, file_path)
+        except shutil.SameFileError:
+            pass
 
         return file_path
 
     def replace_texture(self, texture_name: str, new_texture_path: str) -> str:
-        os.remove(Path(self) / texture_name)
-        new_texture_path = self.add_texture(new_texture_path)
+        file_path = Path(self) / texture_name
+
+        if not self._same_file(file_path, Path(new_texture_path)):
+            os.remove(file_path)
+            new_texture_path = self.add_texture(new_texture_path)
 
         return str(new_texture_path)
 
     def delete(self):
         shutil.rmtree(self)
+
+    @staticmethod
+    def _same_file(src: Path, dst: Path):
+        # Macintosh, Unix.
+        if isinstance(src, os.DirEntry) and hasattr(os.path, 'samestat'):
+            try:
+                return os.path.samestat(src.stat(), os.stat(dst))
+            except OSError:
+                return False
+
+        if hasattr(os.path, 'samefile'):
+            try:
+                return os.path.samefile(src, dst)
+            except OSError:
+                return False
+
+        # All other platforms: check for same pathname.
+        return (os.path.normcase(os.path.abspath(src)) ==
+                os.path.normcase(os.path.abspath(dst)))
 
 
 class CollectionCacheHandler:
